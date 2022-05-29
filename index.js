2cda0aec18b28f3dfffca1cd9cf82f9dd56e2fba
boeunyoon <100947379+boeunyoon@users.noreply.github.com>
2022-05-29 17:37:47 +0900

    proxy, page, Router

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const connect = mongoose.connect(config.mongoURI,
 app.get('/', (req, res) => {
   res.send('Hello World! HI')
 })
-
+app.get('/api/hello', (req, res)=>{
+  res.send("안녕하세요")
+})
 app.post('/register', (req, res) => {
   //회원가입시 필요한 정보들을 client에서 가져오면 
   //그것들을 데이터베이스에 넣어준다
